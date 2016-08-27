46b64cc5c56caa705e4b72ea337615330b85fe94
johndharrison <johndharrison0@gmail.com>
2016-08-27 16:01:08 -0700

    Add information functions

--- R/infoMethods.R
+++ R/infoMethods.R
@@ -1,49 +1,66 @@
 ## infoMethods.R - compiled by RoxygenReady, a package by @vertesy
 
 
-#' getSauceLabsStatus 
-#' 
-#' getSauceLabsStatus <- function(account, ...){
-#' @param account 
-#' @param ... 
+#' Get Sauce Labs Status
+#'
+#' Get the current status of Sauce Labs services
+#' @template account
+#' @template ellipsis
 #' @examples getSauceLabsStatus (account =  , ... =  )
-#' @export 
+#' @export
 
 getSauceLabsStatus <-function (account, ...) {
+	obj <- list()
 	pathTemplate <- whisker.render("https://saucelabs.com/rest/v1/info/status", data = obj)
-	res <- queryAPI(verb = GET, url = build_url(pathURL), source = "getSauceLabsStatus", json = body, 
-		...)
+	pathURL <- parse_url(pathTemplate)
+	res <- queryAPI(verb = GET, account = account, url = build_url(pathURL), source = "getSauceLabsStatus", ...)
+	res
 }
 
 
-#' getSupportedPlatforms 
-#' 
-#' getSupportedPlatforms <- function(account, ...){
-#' @param account 
-#' @param ... 
+#' Get Supported Platforms
+#'
+#' Get a list of objects describing all the OS and browser platforms currently supported on Sauce Labs. Choose the automation API you need, bearing in mind that WebDriver and Selenium RC are each compatible with a different set of platforms.
+#' @template account
+#' @param autoAPI Accepted Values for autoAPI "all", "appium", "webdriver". Defaults to "webdriver"
+#' @template ellipsis
 #' @examples getSupportedPlatforms (account =  , ... =  )
-#' @export 
+#' @export
 
-getSupportedPlatforms <-function (account, ...) {
-	pathTemplate <- whisker.render("https://saucelabs.com/rest/v1/info/platforms/{{automation_api}}", 
+getSupportedPlatforms <-function (account, autoAPI = "webdriver", ...) {
+  obj <- list()
+  obj$automation_api <- autoAPI
+  pathTemplate <- whisker.render("https://saucelabs.com/rest/v1/info/platforms/{{automation_api}}",
 		data = obj)
-	res <- queryAPI(verb = GET, url = build_url(pathURL), source = "getSupportedPlatforms", json = body, 
-		...)
+	pathURL <- parse_url(pathTemplate)
+	res <- queryAPI(verb = GET, account = account, url = build_url(pathURL), source = "getSupportedPlatforms",
+		json = body, ...)
+	rbindlist(res, fill = TRUE)
 }
 
 
-#' getAppiumEolDates 
-#' 
+#' getAppiumEolDates
+#'
 #' getAppiumEolDates <- function(account, ...){
-#' @param account 
-#' @param ... 
+#' @template account
+#' @param username SauceLabs username
+#' @template ellipsis
 #' @examples getAppiumEolDates (account =  , ... =  )
-#' @export 
+#' @export
 
 getAppiumEolDates <-function (account, ...) {
+	obj <- list()
 	pathTemplate <- whisker.render("https://saucelabs.com/rest/v1/info/platforms/appium/eol", data = obj)
-	res <- queryAPI(verb = GET, url = build_url(pathURL), source = "getAppiumEolDates", json = body, 
-		...)
+	pathURL <- parse_url(pathTemplate)
+	res <- queryAPI(verb = GET, account = account, url = build_url(pathURL), source = "getAppiumEolDates",
+		json = body, ...)
+	lapply(res, function(x){
+	  if(!is.null(x)){
+	    as.POSIXct(x, origin="1970-01-01")
+	  }else{
+	    NA
+	  }
+	})
 }
 
 
