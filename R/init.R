85fd7fd8fef23e25c276aa69b66cc4fe3b3e7e92
johndharrison <johndharrison0@gmail.com>
2016-08-28 10:06:18 -0700

    Add example, fix typo, add := from data.table

--- R/init.R
+++ R/init.R
@@ -11,6 +11,7 @@
 #' @importFrom  httr set_config
 #' @importFrom  httr upload_file
 #' @importFrom  data.table rbindlist
+#' @importFrom  data.table :=
 #' @importFrom  xml2 read_html
 #' @importFrom  jsonlite toJSON
 #' @importFrom  jsonlite fromJSON
@@ -21,7 +22,7 @@ NULL
 
 #' Create an account object
 #'
-#' Creates a wrapper around the httr \code{\link{aunthenticate}} function. Adds an "account" class.
+#' Creates a wrapper around the httr \code{\link{authenticate}} function. Adds an "account" class.
 #' @param user The SauceLabs user. By default an environmental variable "SLUSER" is looked for.
 #' @param password The SauceLabs password for user. By default an environmental variable "SLPASS" is looked for.
 #'
@@ -30,6 +31,7 @@ NULL
 #'
 #' @examples
 #' \dontrun{
+#' myAcc <- account()
 #' }
 
 account <- function(user = Sys.getenv("SLUSER"), password = Sys.getenv("SLPASS")){
@@ -44,15 +46,17 @@ account <- function(user = Sys.getenv("SLUSER"), password = Sys.getenv("SLPASS")
 #' \code{queryAPI} A function to send a query to SauceLabs. Intended for seleniumPipes
 #'    internal use mainly.
 #' @param verb The http method to use. See \code{\link{VERB}}
+#' @template account
 #' @param url The url of the remote server endpoint.
 #' @param source The name of the RSauceLabs function that called queryDriver.
-#' @param ...
+#' @template ellipsis
 #'
 #' @return The contents of the response from the remote server. See \code{\link{content}} for details.
 #' @export
 #'
 #' @examples
 #' \dontrun{
+#' # function intended for internal use
 #' }
 queryAPI <- function(verb = GET, account, url, source, ...){
   on.exit(reset_config())
