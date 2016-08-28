e3bb74e5fba063e207b98334675f537b1f52ae0d
johndharrison <johndharrison0@gmail.com>
2016-08-27 19:17:30 -0700

    Add temporary storage functions

--- R/tempStorageMethods.R
+++ R/tempStorageMethods.R
@@ -1,32 +1,47 @@
 ## tempStorageMethods.R - compiled by RoxygenReady, a package by @vertesy
 
 
-#' uploadFile 
-#' 
-#' uploadFile <- function(account, ...){
-#' @param account 
-#' @param ... 
+#' Upload File
+#'
+#' Uploads a file to the temporary sauce storage. The storage will only retain the files for seven days.
+#' @template account
+#' @param username SauceLabs username
+#' @param file file to upload
+#' @param SLfileName name to give the file on SauceLabs. (Defaults to the current name of the file)
+#' @template ellipsis
 #' @examples uploadFile (account =  , ... =  )
-#' @export 
+#' @export
 
-uploadFile <-function (account, ...) {
-	pathTemplate <- whisker.render("https://saucelabs.com/rest/v1/storage/{{username}}/{{your_file_name}}", 
+uploadFile <-function (account, username = Sys.getenv("SLUSER"), file, SLfileName = basename(file), ...) {
+  obj <- list()
+  obj$username <- username
+  obj$your_file_name <- SLfileName
+  pathTemplate <- whisker.render("https://saucelabs.com/rest/v1/storage/{{username}}/{{your_file_name}}",
 		data = obj)
-	res <- queryAPI(verb = POST, url = build_url(pathURL), source = "uploadFile", json = body, ...)
+	pathURL <- parse_url(pathTemplate)
+	body <- upload_file(file)
+	res <- queryAPI(verb = POST, account = account, url = build_url(pathURL), source = "uploadFile",
+		body = body, ...)
+	res
 }
 
 
-#' getStoredFiles 
-#' 
-#' getStoredFiles <- function(account, ...){
-#' @param account 
-#' @param ... 
+#' Get Stored Files
+#'
+#' Check which files are in your temporary storage
+#' @template account
+#' @param username SauceLabs username
+#' @template ellipsis
 #' @examples getStoredFiles (account =  , ... =  )
-#' @export 
+#' @export
 
-getStoredFiles <-function (account, ...) {
+getStoredFiles <-function (account, username = Sys.getenv("SLUSER"), ...) {
+	obj <- list()
+	obj$username <- username
 	pathTemplate <- whisker.render("https://saucelabs.com/rest/v1/storage/{{username}}", data = obj)
-	res <- queryAPI(verb = GET, url = build_url(pathURL), source = "getStoredFiles", json = body, ...)
+	pathURL <- parse_url(pathTemplate)
+	res <- queryAPI(verb = GET, account = account, url = build_url(pathURL), source = "getStoredFiles", ...)
+	res
 }
 
 
